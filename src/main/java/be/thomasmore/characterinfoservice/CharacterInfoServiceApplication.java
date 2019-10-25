391c3ec346cf20f00333d7d41aac02d6967ca056
maartenschroons <r0761272@student.thomasmore.be>
2019-10-25 09:04:45 +0200

    first commit

---
+++ src/main/java/be/thomasmore/characterinfoservice/CharacterInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.characterinfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharacterInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharacterInfoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/thomasmore/characterinfoservice/CharacterInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package be.thomasmore.characterinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharacterInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
