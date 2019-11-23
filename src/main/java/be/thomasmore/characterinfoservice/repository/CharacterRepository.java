5b722a29d092923230c47b18365070c881589832
mnmpower <mnmmaarten@hotmail.com>
2019-11-23 15:15:58 +0100

    added post caracter and get all by name not null

--- src/main/java/be/thomasmore/characterinfoservice/repository/CharacterRepository.java
+++ src/main/java/be/thomasmore/characterinfoservice/repository/CharacterRepository.java
@@ -1,15 +1,29 @@
 package be.thomasmore.characterinfoservice.repository;
 
 import be.thomasmore.characterinfoservice.entity.Character;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+import javax.transaction.Transactional;
+import java.util.List;
+
 @CrossOrigin
-public interface CharacterRepository extends JpaRepository<Character, Integer>{
+@Repository
+@RepositoryRestResource(path = "characters")
+public interface CharacterRepository extends JpaRepository<Character, Integer> {
     Character findCharacterByName(@Param("name") String name);
     Character findCharacterById(@Param("id") Integer id);
+    List<Character> findAllByNameNotNull();
 }
 // http://localhost:8002/characters/search/findCharacterByName?name=valanthe
 // http://localhost:8002/characters/search/findCharacterById?id=1
+// http://localhost:8002/characters/search/findAllByNameNotNull
 
